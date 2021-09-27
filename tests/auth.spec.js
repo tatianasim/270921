26593f27d43a52e11f0e36db78fe72c9b4e21606
TatianaSim <s_tvl@mail.ru>
2021-09-27 19:22:19 +0300

    update auth.spec.js

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -3,27 +3,41 @@ import supertest from 'supertest';
 
 
 describe('auth', function (){
-    const request = supertest('http://paysis.herokuapp.com');
+    let result;
 
-    it('successful log in', function (done){
-        request
-            .post('/auth')
-            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-            .end(function (err, res){
-                expect(res.statusCode).to.eq(200);
-                expect(res.body.token).not .to.be.undefined;
-                done();
+    describe('successful log in', function (){
+        before(function () {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({login: process.env.LOGIN, password: process.env.PASSWORD});
+        });
+
+        it('response status code is 200', function (){
+           result.expect(200);
+        });
+
+        it('response body contains authorization token', function (){
+            result.end(function (err,res){
+                expect(res.body.token).not.to.be.undefined;
             });
+        });
     });
 
-    it('log in wrong credentials should return error', function (done){
-        request
-            .post('/auth')
-            .send({login: 'wrong', password: 'wrong'})
-            .end(function (err, res){
-                expect(res.statusCode).to.eq(404);
+    describe('log in wrong credentials should return error', function (){
+        before(function () {
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({login: 'wrong', password: 'wrong'});
+        });
+
+        it('response status code is 404', function (){
+            result.expect(404);
+        });
+
+        it('response body contains error message', function (){
+            result.end(function (err, res){
                 expect(res.body.message).to.eq('Wrong login or password.');
-                done();
             });
+        });
     });
 });
